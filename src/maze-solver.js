539e79f75761ceababb740bf364083a221296c17
Nirbhay Sibal <nirbhay.sibal@match.com>
2020-11-27 13:30:56 -0600

    find traversable indices

--- src/maze-solver.js
+++ src/maze-solver.js
@@ -4,4 +4,11 @@ module.exports = class MazeSolver extends Grid {
   constructor (arr) {
     super(arr);
   }
+
+  canTraverse (x, y) {
+    if (x < 0 || x >= this.grid.length) return false;
+    if (y < 0 || y >= this.grid[x].length) return false;
+    if (this.grid[x][y] === 0) return false;
+    return true;
+  }
 }
